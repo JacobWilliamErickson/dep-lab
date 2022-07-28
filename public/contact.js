24695ae5c8bbd205919c6f94c5aaad3d4b0008c9
Jacob Erickson <Jakerson8@gmail.com>
2022-07-28 15:07:14 -0600

    aler

--- public/contact.js
+++ public/contact.js
@@ -6,7 +6,7 @@ brokenbtn=()=>{
             .get('/api/contact')
             .then(res => {
             }).catch()
-      }
-}
+            alert("c'mon I said not to click it");
+}}
 let broken = document.querySelector("#broken");
 broken.addEventListener("click", brokenbtn);
